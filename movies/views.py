e961aa1d54f3e94573434af78ff80a8d2d87c7ee
Sudhanshu Badole <sudhanshubadole@gmail.com>
2023-10-19 13:58:12 +0530

    3-Setting Up Views & URLs for API

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Moviedata
+from .serializers import MovieSerializers
 # Create your views here.
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.all()
+    serializer_class = MovieSerializers
